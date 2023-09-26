5a6f669bbacc45aaad887560dda753ad6bca7a32
Iman <iman@Imans-MacBook-Air.local>
2023-09-26 14:10:04 +0300

    middlecheck

--- index.js
+++ index.js
@@ -3,32 +3,72 @@ const endButton = document.querySelector('#endButton');
 const circles = document.querySelectorAll('.circle'); //if you use circles, it'd be one circle only. USE BOOK not BOOKS
 const scoreDisplay = document.querySelector('.score')
 
-
-
+//global variables 
 let score = 0;
+let timer;
+let gamePace = 1000; //you can change it, the lower the faster
+let active = 0;
+let rounds = 0;
 
 const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-console.log(getRndInteger(0, 6));
+//console.log(getRndInteger(0, 3));
 
 const clickCircle = (i) => {
-  console.log('the circle was clicked.', i);
+  if (i !== active) {
+    return endGame()
+  }
+  rounds--
   score += 10
   scoreDisplay.textContent = score
 }
-
 
 //variable name we created is circle. 
 circles.forEach((circle, i) => {
   circle.addEventListener('click', () => clickCircle(i));
 })
+//console.log('Game has started.');
+
+const enableEvents = () => {
+  circles.forEach(circle => {
+    circle.style.pointerEvents = "auto";
+  })
+}
 
 const startGame = () => {
-  console.log('Game has started.');
+  if (rounds >= 3) {
+    return endGame()
+  }
+
+  enableEvents();
+  const newActive = pickNew(active);
+
+  circles[newActive].classList.toggle('active')
+  circles[active].classList.remove('active')
+
+  active = newActive;
+
+  timer = setTimeout(startGame, 1000, gamePace)
+
+  gamePace -= 10
+  rounds++
+  function pickNew(active) {
+    const newActive = getRndInteger(0, 3);
+    if (newActive !== active) {
+      return newActive
+    }
+    return pickNew(active);
+  }
 }
 
 const endGame = () => {
   console.log('Game has ended.');
+  clearTimeout(timer);
+  resetGame();
+}
+
+const resetGame = () => {
+  window.location.reload()
 }
 
 startButton.addEventListener('click', startGame);
