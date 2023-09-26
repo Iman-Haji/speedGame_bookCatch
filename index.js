6b90a7c60e1873147d9c436717d0c142c83240b0
Iman <iman@Imans-MacBook-Air.local>
2023-09-26 12:10:28 +0300

    circles edited

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-//game started
-//game ended
 const startButton = document.querySelector('#startButton');
-const endButton = document.querySelector('#endGame');
+const endButton = document.querySelector('#endButton');
+const circles = document.querySelectorAll('.circle'); //if you use circles, it'd be one circle only. USE BOOK not BOOKS
+const scoreDisplay = document.querySelector('.score')
+
+
+
+let score = 0;
+
+const getRndInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(getRndInteger(0, 6));
+
+const clickCircle = (i) => {
+  console.log('the circle was clicked.', i);
+  score += 10
+  scoreDisplay.textContent = score
+}
+
+
+//variable name we created is circle. 
+circles.forEach((circle, i) => {
+  circle.addEventListener('click', () => clickCircle(i));
+})
 
 const startGame = () => {
   console.log('Game has started.');
@@ -10,8 +30,6 @@ const startGame = () => {
 const endGame = () => {
   console.log('Game has ended.');
 }
-
-endGame
 
 startButton.addEventListener('click', startGame);
 endButton.addEventListener('click', endGame);
