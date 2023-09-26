803f644307f2041e5f55a13ed95644e2fbc21f5c
Iman <iman@Imans-MacBook-Air.local>
2023-09-26 10:27:50 +0300

    start of js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 //game started
 //game ended
-log('game started');
+const startButton = document.querySelector('#startButton');
+const endButton = document.querySelector('#endGame');
+
+const startGame = () => {
+  console.log('Game has started.');
+}
+
+const endGame = () => {
+  console.log('Game has ended.');
+}
+
+endGame
+
+startButton.addEventListener('click', startGame);
+endButton.addEventListener('click', endGame);
